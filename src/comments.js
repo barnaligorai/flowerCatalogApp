6030000c04c8d351cefac56d1e16e5947b493353
Barnali Gorai <99495284+barnaligorai@users.noreply.github.com>
2022-06-28 11:16:16 +0530

    Moved functions related to comments in comments file.

--- src/comments.js
+++ src/comments.js
@@ -1,3 +1,28 @@
+const EOL = '\r\n';
+const pageBreak = '<br/>';
+
+const divBlock = (divClass, body) => `<div class="${divClass}">${body}</div>`;
+
+const parseComment = (commentsString) => {
+  const comments = commentsString.split(EOL);
+  return comments.join(pageBreak);
+};
+
+const formatPost = ({ author, comment, timeStamp }) => {
+  const authorBlock = divBlock('author', `${author} : `);
+  const commentBlock = divBlock('comment', parseComment(comment));
+  const timeStampBlock = divBlock('timeStamp', timeStamp);
+  const contentBlock = divBlock('content', authorBlock + commentBlock);
+  return divBlock('post', timeStampBlock + contentBlock);
+};
+
+const generateCommentsBlock = (comments) => {
+  const commentsHtml = [];
+  const listOfComments = comments.slice(0).reverse();
+  listOfComments.forEach(comment => commentsHtml.push(formatPost(comment)));
+  return commentsHtml.join('');
+};
+
 class Comments {
   #comments;
   constructor(comments) {
@@ -10,6 +35,10 @@ class Comments {
 
   getComments() {
     return this.#comments.slice(0);
+  }
+
+  toHtml() {
+    return generateCommentsBlock(this.getComments());
   }
 }
 
--- src/serveGuestBook.js
+++ src/serveGuestBook.js
@@ -1,30 +1,11 @@
-const EOL = '\r\n';
-const pageBreak = '<br/>';
-
-const divBlock = (divClass, body) => `<div class="${divClass}">${body}</div>`;
-
-const parseComment = (commentsString) => {
-  const comments = commentsString.split(EOL);
-  return comments.join(pageBreak);
-};
-
-const formatPost = ({ author, comment, timeStamp }) => {
-  const authorBlock = divBlock('author', `${author} : `);
-  const commentBlock = divBlock('comment', parseComment(comment));
-  const timeStampBlock = divBlock('timeStamp', timeStamp);
-  const contentBlock = divBlock('content', authorBlock + commentBlock);
-  return divBlock('post', timeStampBlock + contentBlock);
-};
-
 const serveGuestBook = (response, comments, { readFile }, sourceDir) => {
   const fileName = sourceDir + '/guestbookTemplate.txt';
   const template = readFile(fileName, 'utf8');
-  const commentsHtml = [];
-  comments.getComments().reverse().forEach(comment => {
-    commentsHtml.push(formatPost(comment));
-  });
+  // const listOfComments = comments.getComments();
+  // const commentsBlock = generateCommentsBlock(listOfComments);
 
-  response.send(template.replace('__COMMENTS__', commentsHtml.join('')));
+  // response.send(template.replace('__COMMENTS__', commentsBlock));
+  response.send(template.replace('__COMMENTS__', comments.toHtml()));
 };
 
 module.exports = { serveGuestBook };
