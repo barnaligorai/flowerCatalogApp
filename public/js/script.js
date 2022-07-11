ce7738cb076f5ae1bebe38cdd290be3ff46b661b
Barnali Gorai <99495284+barnaligorai@users.noreply.github.com>
2022-07-11 10:14:38 +0530

    Renamed appendComment to prependComment and refactored

--- public/js/script.js
+++ public/js/script.js
@@ -1,49 +1,82 @@
-const appendComment = function () {
-  // this function should accept comment to add.
-  const allComments = document.querySelector('.comments');
-  const comment = JSON.parse(this.response);
-  const postElement = document.createElement('div');
-  postElement.className = 'post';
-  const timeStampElement = document.createElement('div');
-  timeStampElement.className = 'timeStamp'
-  timeStampElement.innerText = comment.timeStamp;
-  const contentElement = document.createElement('div');
-  contentElement.className = 'content';
+const createName = ({ name }) => {
   const nameDiv = document.createElement('div');
   nameDiv.className = 'name';
-  nameDiv.innerText = comment.name;
+  nameDiv.innerText = name;
+  return nameDiv;
+};
+
+const createComment = ({ comment }) => {
   const commentDiv = document.createElement('div');
   commentDiv.className = 'comment';
-  commentDiv.innerText = comment.comment;
-  postElement.appendChild(timeStampElement);
+  commentDiv.innerText = comment;
+  return commentDiv;
+};
+
+const createContent = (comment) => {
+  const contentElement = document.createElement('div');
+  contentElement.className = 'content';
+
+  const nameDiv = createName(comment);
+  const commentDiv = createComment(comment);
+
   contentElement.appendChild(nameDiv);
   contentElement.appendChild(commentDiv);
+
+  return contentElement;
+};
+
+const createTimeStamp = ({ timeStamp }) => {
+  const timeStampElement = document.createElement('div');
+  timeStampElement.className = 'timeStamp';
+  timeStampElement.innerText = timeStamp;
+  return timeStampElement;
+};
+
+const createPost = (comment) => {
+  const postElement = document.createElement('div');
+  postElement.className = 'post';
+  const timeStampElement = createTimeStamp(comment);
+  const contentElement = createContent(comment);
+
+  postElement.appendChild(timeStampElement);
   postElement.appendChild(contentElement);
+  return postElement;
+};
+
+const prependComment = function ({ response }) {
+  // this function should accept comment to add.
+  const comment = JSON.parse(response);
+  const allComments = document.querySelector('.comments');
+  const postElement = createPost(comment);
   allComments.prepend(postElement);
 };
 
 const sendRequest = (method, url, body = '', callBack) => {
   const xhr = new XMLHttpRequest();
-  xhr.onload = callBack;
+  xhr.onload = () => callBack(xhr);
   xhr.open(method, url);
   xhr.send(body);
 };
 
 const addComment = (event) => {
-  console.log('Inside add comment');
   const formElement = document.querySelector('form');
   const formData = new FormData(formElement);
   const parsedForm = new URLSearchParams(formData);
-  sendRequest('POST', '/add-comment', parsedForm, appendComment);
+  sendRequest('POST', '/add-comment', parsedForm, prependComment);
+  formElement.reset();
   // add comment to json + empty response + all data should be here.
   // Request for rest of data through API.
-}
+};
 
 const main = () => {
   // ask for current comments here.
   const buttonElement = document.querySelector('#submit');
-  console.log(buttonElement);
   buttonElement.onclick = addComment;
 };
 
 window.onload = main;
+
+
+
+
+
